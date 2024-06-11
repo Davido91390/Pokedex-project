0bba7dde8e38cd0418616fee0fcf22179a3fbb67
Davido91390 <david.kabore@hotmail.fr>
2024-06-11 17:02:24 +0200

    Ajout tableau Pokemon

--- Pokedex/src/components/PokemonCard.jsx
+++ Pokedex/src/components/PokemonCard.jsx
@@ -1,7 +1,28 @@
-function PokemonCard () {
-    return <figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" />
-        <figcaption>Bulbasaur</figcaption>
+function PokemonCard() {
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
+  const pokemon = pokemonList[0];
+
+  return (
+    <figure>
+      <figcaption>{pokemon.name}</figcaption>
+
+      {pokemon.imgSrc ? (
+        <img src={pokemon.imgSrc} alt={pokemon.name} />
+      ) : (
+        <p>???</p>
+      )}
     </figure>
+  );
 }
+
 export default PokemonCard;
