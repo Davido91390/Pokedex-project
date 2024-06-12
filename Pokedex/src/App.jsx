584536897fc2dbb7caf6374aba027eb9e7dafb91
Davido91390 <david.kabore@hotmail.fr>
2024-06-12 13:58:39 +0200

    les porps

--- Pokedex/src/App.jsx
+++ Pokedex/src/App.jsx
@@ -1,21 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import PokemonCard from './components/PokemonCard'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import PokemonCard from "./components/PokemonCard";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
 
   return (
     <>
-  
-      <div>
-        
-        <PokemonCard/>
-    </div>
+      <PokemonCard pokemon={pokemonList[1]} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- Pokedex/src/components/PokemonCard.jsx
+++ Pokedex/src/components/PokemonCard.jsx
@@ -1,17 +1,5 @@
-function PokemonCard() {
-  const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
-
-  const pokemon = pokemonList[0];
-
+function PokemonCard({ pokemon }) {
+  console.log(pokemon);
   return (
     <figure>
       <figcaption>{pokemon.name}</figcaption>
